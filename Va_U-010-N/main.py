257359f02e25888c1870f0fceb5fb4d12c3e6ca6
val <val@natmatch.com>
2022-10-27 00:00:08 -0400

    First run

--- Va_U-010-N/main.py
+++ Va_U-010-N/main.py
@@ -10,7 +10,10 @@ from VaData import VaData
 va_data = VaData()
 VaConfig.setup(va_data)
 local_data = VaData()
+
 VaConfigLocal.setup(local_data)
+
+local_data.set('Input array...M', [2, -3 , 3])
 
 test = va_data.getAll()
 print(test)
@@ -18,8 +21,10 @@ print('------------------------')
 test = local_data.getAll()
 print(test)
 print('------------------------')
-#VaBox.start(va_data,local_data)
 
+VaBox.start(va_data,local_data)
+
+print(local_data.get('The sum of elements of array...sum'))
 
 print('\nThe end')
 
